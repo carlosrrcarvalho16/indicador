d52a12c9c96884faf8ef26981348600d84d5af6b
Carlos Carvalho <carlosrrcarvalho16@gmail.com>
2018-04-07 20:22:40 -0300

    Ajustado _departament - identação

--- backend/views/site/_departamentos.php
+++ backend/views/site/_departamentos.php
@@ -12,23 +12,22 @@ foreach($departaments as $departament) {
       $percent = intval($percent * 100);
 ?>
 <div class="col-md-5 col-sm-4 col-xs-12">
-          <div class="info-box bg-aqua">
-            <span class="info-box-icon">
-              <i class="ion ion-stats-bars"></i>
-            </span>
-            <div class="info-box-content">
-              <span class="info-box-text"><?php echo $departament->departamento; ?></span>
-              <span class="info-box-number"><?php echo $departament->qtd_preenchida; ?> / <?php echo $departament->qtd_ind; ?> - <?php echo $percent ;?>% Preenchido(s)</span>
-              <div class="progress">
-                
-                <div class="progress-bar" style="width: <?php echo $percent; ?>%"></div>
-              </div>  
-                <a href="<?php echo BaseUrl::base()?>/departament?id=<?php echo $departament->id ?>" class="small-box-footer">Ver Indicadores <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
+  <div class="info-box bg-aqua">
+    <span class="info-box-icon">
+      <i class="ion ion-stats-bars"></i>
+    </span>
+    <div class="info-box-content">
+      <span class="info-box-text"><?php echo $departament->departamento; ?></span>
+      <span class="info-box-number"><?php echo $departament->qtd_preenchida; ?> / <?php echo $departament->qtd_ind; ?> - <?php echo $percent ;?>% Preenchido(s)</span>
+    <div class="progress">            
+      <div class="progress-bar" style="width: <?php echo $percent; ?>%"></div>
+    </div>  
+      <a href="<?php echo BaseUrl::base()?>/departament?id=<?php echo $departament->id ?>" class="small-box-footer">Ver Indicadores <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+    <!-- /.info-box-content -->
+  </div>
+  <!-- /.info-box -->
+</div>
 
 
 
