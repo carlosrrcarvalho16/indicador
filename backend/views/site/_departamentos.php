0d92bbb8fb3c1b058ac9d5584e24eb7f95cc87c4
Carlos Carvalho <carlosrrcarvalho16@gmail.com>
2018-04-07 20:19:11 -0300

    Ajustado _departament

--- backend/views/site/_departamentos.php
+++ backend/views/site/_departamentos.php
@@ -2,21 +2,38 @@
 
 /* @var $this yii\web\View */
 use yii\helpers\BaseUrl;
-
-foreach($departaments as $departament) {
-	$departament = (object) $departament; //força o tipo da variável
 ?>
-<div class="col-lg-4 col-xs-6">
-  <!-- small box -->
-  <div class="small-box bg-aqua">
-    <div class="inner">
-      <h1><?php echo $departament->departamento; ?></h1> 
-      <h3><?php echo $departament->qtd_preenchida; ?> / <?php echo $departament->qtd_ind; ?></h3> 
-    </div>
-    <div class="icon">
-      <i class="ion ion-pie-graph"></i>
-    </div>
-    <a href="<?php echo BaseUrl::base()?>/departament?id=<?php echo $departament->id ?>" class="small-box-footer">Ver Indicadores <i class="fa fa-arrow-circle-right"></i></a>
-  </div>
-</div>
-<?php } ?>
+<section class="content">
+<?php
+foreach($departaments as $departament) {
+  $departament = (object) $departament; //força o tipo da variável
+?>
+<?php $percent = (($departament->qtd_preenchida)/($departament->qtd_ind));
+      $percent = intval($percent * 100);
+?>
+<div class="col-md-5 col-sm-4 col-xs-12">
+          <div class="info-box bg-aqua">
+            <span class="info-box-icon">
+              <i class="ion ion-stats-bars"></i>
+            </span>
+            <div class="info-box-content">
+              <span class="info-box-text"><?php echo $departament->departamento; ?></span>
+              <span class="info-box-number"><?php echo $departament->qtd_preenchida; ?> / <?php echo $departament->qtd_ind; ?> - <?php echo $percent ;?>% Preenchido(s)</span>
+              <div class="progress">
+                
+                <div class="progress-bar" style="width: <?php echo $percent; ?>%"></div>
+              </div>  
+                <a href="<?php echo BaseUrl::base()?>/departament?id=<?php echo $departament->id ?>" class="small-box-footer">Ver Indicadores <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+
+
+
+
+<?php } 
+?>
+
+</section>
