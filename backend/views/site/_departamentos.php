47985e724b9283f7fd353dd8215375a75ca97289
Carlos Carvalho <carlosrrcarvalho16@gmail.com>
2018-04-27 13:06:25 -0300

    Ajustado Layout site/index

--- backend/views/site/_departamentos.php
+++ backend/views/site/_departamentos.php
@@ -11,7 +11,7 @@ foreach($departaments as $departament) {
 <?php $percent = (($departament->qtd_preenchida)/($departament->qtd_ind));
       $percent = intval($percent * 100);
 ?>
-<div class="col-md-5 col-sm-4 col-xs-10">
+<div class="col-md-6">
   <div class="info-box bg-aqua">
     <span class="info-box-icon">
       <i class="fa <?php echo $departament->icons; ?>"></i>
@@ -101,11 +101,12 @@ foreach ($selectQtdDepartamentoPlanosAcao as $value) {
 <!-- Small boxes (Stat box) -->
 <section class="content">
     <div class="row">
-        <div class="col-md-5">
+        <div class="col-md-6">
             <!-- DONUT CHART -->
             <div class="box box-danger">
                 <div class="box-header with-border">
                     <h3 class="box-title">Status dos planos de ação</h3>
+
                     <div class="box-tools pull-right">
                         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                         </button>
@@ -113,12 +114,12 @@ foreach ($selectQtdDepartamentoPlanosAcao as $value) {
                     </div>
                 </div>
                 <div class="box-body">
-                    <canvas id="myChartNCatrazadas" style="height:250px"></canvas>
+                    <canvas id="pieChart" style="height:250px"></canvas>
                 </div>
             </div>
 
         </div>
-        <div class="col-md-5">
+        <div class="col-md-6">
             <!-- DONUT CHART -->
             <div class="box box-danger">
                 <div class="box-header with-border">
@@ -157,7 +158,7 @@ foreach ($selectQtdDepartamentoPlanosAcao as $value) {
         //Number - The width of each segment stroke
         segmentStrokeWidth   : 2,
         //Number - The percentage of the chart that we cut out of the middle
-        percentageInnerCutout: 50, // This is 0 for Pie charts
+        percentageInnerCutout: 500, // This is 0 for Pie charts
         //Number - Amount of animation steps
         animationSteps       : 100,
         //String - Animation easing effect
@@ -170,12 +171,13 @@ foreach ($selectQtdDepartamentoPlanosAcao as $value) {
         responsive           : true,
         // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
         maintainAspectRatio  : true,
+        legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>',
     }
 
 
 
-    var ctx = document.getElementById("myChartNCatrazadas").getContext('2d');
-    var myChartNCatrazadas = new Chart(ctx, {
+    var ctx = document.getElementById("pieChart").getContext('2d');
+    var pieChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
             labels: ['No praso','Atrasada'],
